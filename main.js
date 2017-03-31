8dfd7a5f99e0ecdc1b051ce62e642e1ab76b92d4
Erick G <lmterickg@cox.net>
2017-03-31 10:14:27 -0700

    March commit

--- main.js
+++ main.js
@@ -1,20 +1,32 @@
 $(document).ready(function(){
 
-	/*$("#add").on('click', function(){
+	$("#add").on('click', function(){
 		var value1 = $("#value1").val()
 		var value2 = $("#value2").val()
 		$("#answer").val(Number(value1) + Number(value2))
-	})*/
+	})
 
+	$("#divide").on('click', function(){
+		var value1 = $("#value1").val()
+		var value2 = $("#value2").val()
+		$("#answer").val(Number(value1) / Number(value2))
+	})
 
+	$("#multiply").on('click', function(){
+		var value1 = $("#value1").val()
+		var value2 = $("#value2").val()
+		$("#answer").val(Number(value1) * Number(value2))
+	})
 
+	$("#subtract").on('click', function(){
+		var value1 = $("#value1").val()
+		var value2 = $("#value2").val()
+		$("#answer").val(Number(value1) - Number(value2))
+	})
 
-
-
-
-
-
-
+	$("#clear").on('click', function(){
+		$("#answer").detach(Number(value1) || Number(value2))
+	})
 
 
 
